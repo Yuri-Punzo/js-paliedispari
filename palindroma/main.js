3c1bff88de17da6cc40ed8a42ee75ef08cde01ea
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-14 13:57:17 +0200

    intial commit for "palindromo"

---
+++ palindroma/main.js
@@ -0,0 +1,4 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+const userWord = prompt("Inserisci una parola, ti dirò se è palindroma o no");
