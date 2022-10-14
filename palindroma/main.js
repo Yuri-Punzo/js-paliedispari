02d9d5667984d3ac66111e11240c977eda798603
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-14 17:05:06 +0200

    restart con ciclo invece di split reverse join

--- palindroma/main.js
+++ palindroma/main.js
@@ -5,22 +5,27 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 const userWord = prompt("Inserisci una parola, ti dirò se è palindroma o no");
 console.log(userWord);
 
-/* creare un array dalla stringa che abbia come elementi singoli le lettere che la compongono
-ribaltare l'array
-ricomporre le lettere ormai rovesciate della parseFloat
+// VERSIONE SCOMPOSTA SENZA SPLIT/REVERSE/JOIN MA CON 
 
-checkare se è ancora uguale a se stessa */
-
-
-
-
-
-
+let text = userWord
+let i=0
+while(i < text.length) {
+    let char = text.charAt(i);    
+    console.log(char);
+    i++
+}
 
 
 
 
-/* VERSIONE CON SPLIT/REVERSE/JOIN
+
+
+
+
+
+
+/* //VERSIONE RAPIDA CON SPLIT/REVERSE/JOIN
+
 const userWord = prompt("Inserisci una parola, ti dirò se è palindroma o no");
 console.log(userWord);
 
@@ -29,13 +34,15 @@ function wordTester(word) {
     const splitString = word.split("");// const splitString = "hello".split("");
     // ["h", "e", "l", "l", "o"]
 
+
     // Step 2. Use the reverse() method to reverse the new created array
     const reverseArray = splitString.reverse();
     // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
     // ["o", "l", "l", "e", "h"]
 
     // Step 3. Use the join() method to join all elements of the array into a string
-    const joinArray = reverseArray.join("");
+    const joinArray = reverseArray.join("g");
+    console.log(joinArray);
     // var joinArray = ["o", "l", "l", "e", "h"].join("");
     // "olleh"
     console.log(joinArray);
